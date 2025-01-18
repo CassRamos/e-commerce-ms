672ead233fefa59b5050e35384167db27953c145
Cass <cassianoleaoramos@gmail.com>
2025-01-18 20:45:20 -0300

    Create and configure product service

---
+++ services/product/src/main/java/cass/product/ProductApplication.java
@@ -0,0 +1,13 @@
+package cass.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+
+}
